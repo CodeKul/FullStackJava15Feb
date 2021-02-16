32f4f4d81575bb5c56b0315fa1748fcfd3fb5063
vaibhav <codekul.vaibhav@gmail.com>
2021-02-16 16:24:26 +0530

    First Commit

---
+++ src/Sample.java
@@ -0,0 +1,43 @@
+public class Sample{
+
+    //primitive datatype
+    int i=0;
+    long l = 0L;
+    char c= 'C';
+    boolean b = false;
+    float f= 0.1F;
+    double d = 0.2;
+    byte bt = 9;
+    short s = 1;
+
+
+    //non primitive datatypes
+
+    Integer integer =9;
+    Long aLong= 9L;
+    Short aShort = 0;
+    Float aFloat = 6.8F;
+    Double dADouble = 12.0;
+    Character character ='c';
+    Boolean aBoolean =false;
+    Byte aByte = 21;
+
+    String s1 ="Sahil";
+
+    public void show(){
+        System.out.println("Hello");
+    }
+
+    public int dispaly(){
+       return 3;
+    }
+    //public - access modifier
+    //static - JVM instantiate without creating the object of class
+
+    public static void main(String[] args) {
+
+        Sample sample = new Sample();
+        sample.show();
+    }
+
+}
