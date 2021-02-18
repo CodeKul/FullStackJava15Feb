7ef723418c90020bfe272e106e85458ff1559877
vaibhav <codekul.vaibhav@gmail.com>
2021-02-18 17:11:29 +0530

    add method example

--- src/polymorphism/MethodOverloadingDemo.java
+++ src/polymorphism/MethodOverloadingDemo.java
@@ -1,32 +1,48 @@
 package polymorphism;
 
+
 public class MethodOverloadingDemo {
 
+
+    int c=0;
     public static void main(String[] args) {
         MethodOverloadingDemo demo = new MethodOverloadingDemo();
-
-        demo.show(1);
-
+        demo.add(1,2,9,0);
     }
 
-    public int show() {
+    public int showDisplay() {
         return 1;
     }
 
     public void show(int i) {
         System.out.println("In show 2");
+
     }
+
+    public void add(int i , int j){
+        c=i+j;
+    }
+
+    public void add(int i,int j , int k){
+        c = i+j+k;
+    }
+
+     public void add(int i,int j , int k,int l){
+        c = i+j+k+l;
+    }
+
+
 }
 
-class MethodOverriding extends MethodOverloadingDemo{
-
-    public void show(int i) {
-        System.out.println("In Method overriding");
-    }
+class MethodOverriding extends MethodOverloadingDemo {
 
     public static void main(String[] args) {
         MethodOverriding methodOverriding = new MethodOverriding();
 
         methodOverriding.show(9);
+    }
+
+    public void show(int i) {
+        System.out.println("In Method overriding");
     }
 }
