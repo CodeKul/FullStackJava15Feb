76fc86bda7756c651a41e492e50894e564817034
vaibhav <codekul.vaibhav@gmail.com>
2021-02-26 16:57:44 +0530

    While loop and do while loop

---
+++ src/control_statment/ScannerDemo.java
@@ -0,0 +1,18 @@
+package control_statment;
+
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+
+    public static void main(String[] args) {
+
+        int i;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number");
+        i = scanner.nextInt();
+
+        System.out.println("i value"+i);
+    }
+}
---
+++ src/control_statment/WhileLoopDemo.java
@@ -0,0 +1,24 @@
+package control_statment;
+
+public class WhileLoopDemo {
+
+    public static void main(String[] args) {
+
+        int i =(int) (Math.random()*100+1);
+
+        while (i<=10) {
+            System.out.println("i is positive");
+            i++;
+        }
+
+//        while(true){
+//            System.out.println("infinite");
+//        }
+
+        do {
+            System.out.println("i");
+            i++;
+        }while (i<=10);
+
+    }
+}
