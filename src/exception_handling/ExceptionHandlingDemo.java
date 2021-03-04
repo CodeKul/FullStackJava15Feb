f90fe3127ede2430e63fbf04e2a0ba56446ea351
vaibhav <codekul.vaibhav@gmail.com>
2021-03-04 16:50:35 +0530

    StackOverFlowError and Throw keyword

--- src/exception_handling/ExceptionHandlingDemo.java
+++ src/exception_handling/ExceptionHandlingDemo.java
@@ -4,20 +4,25 @@ public class ExceptionHandlingDemo {
 
     public static void main(String[] args) {
 
-        int i=10;
+        int i = 10;
 
-        try{
-//            int j = i/0;
-
-            String s = null;
-            System.out.println(s.length());
+        try {
+//            try {
+//                int j = i / 0;
+//            } catch (Exception e) {
+//                System.out.println(e);
+//            }
+//            String s = null;
+//            System.out.println(s.length());
 //            System.out.println(j);
 
-        }catch (Exception e){
-            System.out.println("runtime=>"+e.getMessage());
+        } catch (Exception e) {
+            System.out.println("runtime=>" + e.getMessage());
+        } finally {
+
+            System.out.println("In Finally");
         }
 
-        System.out.println("Hello");
 
     }
 }
